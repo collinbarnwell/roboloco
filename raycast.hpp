d2c40784af14806db83ac9d28de6b5278c03a476
collinbarnwell <steezmeister@gmail.com>
2015-06-07 22:38:34 -0500

    fixed analytic raycast

--- raycast.hpp
+++ raycast.hpp
@@ -25,111 +25,93 @@ void TrimOcclusion(PointXY x, Line &occludee, Line occluder, vector<Line> &lineL
     if (occluder.isZero()) return;
 
     vector<PointXY> pois;
-    PointXY intersection, poi1, poi2, ignore;
+    PointXY poi1, poi2;
 
-    Line to_occludee_start(x, occludee.getStart());
-    Line to_occludee_end(x, occludee.getEnd());
     Line to_occluder_start(x, occluder.getStart());
     Line to_occluder_end(x, occluder.getEnd());
 
-    if (to_occludee_start.intersect(occluder, ignore) && to_occludee_end.intersect(occluder, ignore)) {
-        // line is totally occluded`
-        occludee.set_to_zero();
-        return;
-    }
-
-    // if (occludee.intersect(occluder, intersection)) {
-    //     pois.push_back(intersection);
-    //     cout << "should not happen" << endl;
-    //     throw 1;
-    // }
-    if (to_occluder_start.intersectOutOfBound(occludee, poi1)) {
+    if (to_occluder_start.intersectOutOfBound(occludee, poi1))
         pois.push_back(poi1);
-    }
-    if (to_occluder_end.intersectOutOfBound(occludee, poi2)) {
+    if (to_occluder_end.intersectOutOfBound(occludee, poi2))
         pois.push_back(poi2);
-    }
 
-    // if at least one endpoint is clear ^^
-    // and there are no pois ^
-    // occludee is not occluded
-    if (pois.size() < 1) {
-        return;
-    }
+    pois.push_back(occludee.getStart());
+    pois.push_back(occludee.getEnd());
 
-    // 1) find unoccluded endpt, keep until closest poi/endpt 
-    // 2) throw away until following poi/endpt
-    // 3) repeat...
+    PointSorter psort(occludee.getEnd());
+    sort(pois.begin(), pois.end(), psort);
 
-    if (to_occludee_start.intersect(occluder, ignore)) 
-    // start is occluded
-    { 
-        // start at end
-        PointSorter psort(occludee.getEnd());
-        sort(pois.begin(), pois.end(), psort);
+    PointXY current = pois.back();
+    pois.pop_back();
+    PointXY next = pois.back();
+    pois.pop_back();
 
-        // for (int i = 0; i < pois.size(); i++) {
-        //     cout << pois[i].x << ", " << pois[i].y << endl;
-        // }
+    // Loop through pois-separated segments
+    bool first_keeper = true;
+    while (true) {
+        bool last_iteration = false;
+        if (pois.size() == 0)
+            last_iteration = true;
 
-        occludee.setStart(pois.back());
-        pois.pop_back();
-    } 
-    else 
-    // start is not occluded
-    {
-        // add end to pois if also not occluded
-        if (!to_occludee_end.intersect(occluder, ignore)) {
-            pois.push_back(occludee.getEnd());
+        // find point half way between current and next
+        PointXY midpoint, ignore;
+        midpoint.x = (current.x + next.x)/2.0;
+        midpoint.y = (current.y + next.y)/2.0;
+        Line l = Line(x, midpoint);
+
+        if (first_keeper) 
+        {
+            // check for occluder intersection in line to midpoint of occludee section
+            if (l.intersect(occluder, ignore)) {
+                if (last_iteration) // nothing is un-occluded
+                    occludee.set_to_zero();
+                // else segment has already been trimmed and nothing needs to be added
+            }
+            else {
+                occludee.setStart(current);
+                occludee.setEnd(next);
+                first_keeper = false;
+            }
+        } 
+        else 
+        {
+            if (!l.intersect(occluder, ignore))
+                lineList.push_back(Line(current, next));
+            // else segment has already been trimmed, do nothing
         }
 
-        // start at start
-        PointSorter psort(occludee.getStart());
-        sort(pois.begin(), pois.end(), psort);
+        if (last_iteration)
+            return;
 
-        occludee.setEnd(pois.back());
+        current = next;
+        next = pois.back();
         pois.pop_back();
     }
-
-    // add possible disconnected segment to lineList
-    if (pois.size() > 1) {
-        PointXY new_line_start = pois.back();
-        pois.pop_back();
-        Line new_line(new_line_start, pois.back());
-        lineList.push_back(new_line);
-    }
-    
-    return;
 }
 
 
-vector<Line> AnalyticRayCast(PointXY x, vector<Line> map) {
-
-    int map_size = map.size();
-
+vector<Line> AnalyticRayCast(PointXY x, vector<Line> map) 
+{
+    vector<Line> Lprime(map);
     vector<Line> L;
 
-    for (int i = 0; i < map_size; i++)
+    for (int i = 0; i < Lprime.size(); i++)
     {
-        Line currentLine = map[i];
+        Line li = Lprime[i];
 
-        if (currentLine.isZero()) { continue; }
-
-        for (int j = 0; j < map_size; j++)
-        {
-            if (j == i) { continue; }
-
-            TrimOcclusion(x, currentLine, map[j], map);
+        for (int j = 0; j < L.size(); j++) {
+            Line lj = L[j];
+            TrimOcclusion(x, li, lj, Lprime);
         }
 
-        if (currentLine.isZero()) { continue; }
+        if (li.isZero()) { continue; }
 
-        int lsize = L.size();
-        for (int j = 0; j < lsize; j++) {
-            TrimOcclusion(x, L[j], currentLine, map);
+        for (int j = 0; j < L.size(); j++) {
+            Line lj = L[j];
+            TrimOcclusion(x, lj, li, Lprime);
         }
 
-        L.push_back(currentLine);
+        L.push_back(li);
     }
 
     return L;
